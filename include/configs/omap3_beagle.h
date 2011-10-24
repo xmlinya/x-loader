c88d5ed93f62ae9def18436c95b093d780a5695b
John Rigby <john.rigby@linaro.org>
2011-10-23 23:09:34 -0600

    OMAP3: Beagle: Add standard addresses to env
    Signed-off-by: John Rigby <john.rigby@linaro.org>

--- include/configs/omap3_beagle.h
+++ include/configs/omap3_beagle.h
@@ -276,6 +276,10 @@
 	"ramboot=echo Booting from ramdisk ...; " \
 		"run ramargs; " \
 		"bootm ${loadaddr}\0" \
+	"kernel_addr_r=0x88000000\0" \
+	"ramdisk_addr_r=0x81600000\0" \
+	"pxefile_addr_r=0x86000000\0"
+
 
 #define CONFIG_BOOTCOMMAND \
 	"if mmc rescan ${mmcdev}; then " \
