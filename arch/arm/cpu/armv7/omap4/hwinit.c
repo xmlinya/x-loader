c567c2932860c82473242e636977317c65b06f14
Aneesh V <aneesh@ti.com>
2011-11-02 16:38:05 +0530

    omap4: fix IO setting
    Override CONTROL_EFUSE_2_OVERRIDE unconditionally with hw
    team recommended value. Presently, the register is overridden
    only if the POR value is 0. POR value is not working on some
    of the chips.
    
    Signed-off-by: Aneesh V <aneesh@ti.com>

--- arch/arm/cpu/armv7/omap4/hwinit.c
+++ arch/arm/cpu/armv7/omap4/hwinit.c
@@ -113,8 +113,7 @@ void do_io_settings(void)
 	if ((omap4_rev < OMAP4460_ES1_0) || !readl(&ctrl->control_efuse_1))
 		writel(CONTROL_EFUSE_1_OVERRIDE, &ctrl->control_efuse_1);
 
-	if (!readl(&ctrl->control_efuse_2))
-		writel(CONTROL_EFUSE_2_OVERRIDE, &ctrl->control_efuse_2);
+	writel(CONTROL_EFUSE_2_OVERRIDE, &ctrl->control_efuse_2);
 }
 #endif
 
