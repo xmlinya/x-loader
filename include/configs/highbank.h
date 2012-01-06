497b7612ecd89b84ff781b2ee1fba5031f7d89a0
Jason Hobbs <jason.hobbs@calxeda.com>
2012-01-06 07:46:11 -0600

    configs/highbank.h: correct the fdt_addr env variable
    its name didn't match what cmd_pxe wanted, still.

--- include/configs/highbank.h
+++ include/configs/highbank.h
@@ -94,7 +94,7 @@
 #define CONFIG_SYS_LOAD_ADDR		0x800000
 
 #define CONFIG_EXTRA_ENV_SETTINGS	\
-		"fdtaddr_r=0x600000\0" \
+		"fdt_addr=0x600000\0" \
 		"pxefile_addr_r=0x700000\0" \
 		"kernel_addr_r=0x800000\0" \
 		"ramdisk_addr_r=0x01000000\0" \
